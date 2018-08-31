20d5ba972f4fe9cd7ebce931939792ab7046df4b
C.A.M. Gerlach <widenetservices@gmail.com>
2018-08-30 22:01:17 -0500

    Add basic __init__ and _version files with headers

---
+++ gettext_helpers/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""
+Gettext-Helpers
+===============
+
+Copyright (c) 2018- Gettext-Helpers Contributors (see AUTHORS.txt)
+
+This file is part of
+`Gettext-Helpers <https://github.com/spyder-ide/gettext-helpers>`__.
+
+Gettext-Helpers is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Gettext-Helpers is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Gettext-Helpers.  If not, see <https://www.gnu.org/licenses/>.
+
+See LICENSE.txt in the gettext-helpers repository root for the full text.
+See `NOTICE.txt <https://github.com/spyder-ide/gettext-helpers/blob/master/NOTICE.txt>`__
+in the same location for more detailed legal history.
+"""
+
+from gettext_helpers._version import __version__
---
+++ gettext_helpers/_version.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2018- Gettext-Helpers Contributors (see AUTHORS.txt)
+#
+# This file is part of Gettext-Helpers.
+# <https://github.com/spyder-ide/gettext-helpers>
+#
+# Gettext-Helpers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gettext-Helpers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gettext-Helpers.  If not, see <https://www.gnu.org/licenses/>.
+#
+# See LICENSE.txt in the gettext-helpers repository root for the full text.
+# Also, see NOTICE.txt in the same location for more detailed legal history:
+# <https://github.com/spyder-ide/gettext-helpers/blob/master/NOTICE.txt>
+# -----------------------------------------------------------------------------
+
+
+"""Gettext-Helpers version file."""
+
+VERSION_INFO = (0, 1, 0, 'dev0')
+__version__ = '.'.join(map(str, VERSION_INFO))
