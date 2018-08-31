c2bd85ad60dc33022c8ef36c6c2452a187b0cb9d
C.A.M. Gerlach <widenetservices@gmail.com>
2018-08-30 21:49:12 -0500

    Add updated file headers

--- gettext_scan.py
+++ gettext_scan.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2009-2010 Pierre Raybaut/CEA (CeCILL License Version 2)
+# Copyright (c) 2011- Gettext-Helpers Contributors (see AUTHORS.txt)
 #
-# Copyright © Spyder Project Contributors
-# Licensed under the terms of the MIT License
-# (see spyder/__init__.py for details)
+# This file is part of Gettext-Helpers.
+# <https://github.com/spyder-ide/gettext-helpers>
+#
+# Gettext-Helpers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Gettext-Helpers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gettext-Helpers.  If not, see <https://www.gnu.org/licenses/>.
+#
+# See LICENSE.txt in the gettext-helpers repository root for the full text.
+# Also, see NOTICE.txt in the same location for more detailed legal history:
+# <https://github.com/spyder-ide/gettext-helpers/blob/master/NOTICE.txt>
+# -----------------------------------------------------------------------------
+
 
 from gettext_helpers.gettext_helpers import do_rescan, do_rescan_files
 
